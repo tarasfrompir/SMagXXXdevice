389ffe23396964e3b5a5ee95aecca0c08a6052a4
tarasfrompir <tarasfrompir@inbox.ru>
2019-01-24 20:50:00 +0200

    Add files via upload

--- modules/devices/SMagXXXdevice_app.php
+++ modules/devices/SMagXXXdevice_app.php
@@ -1,7 +1,7 @@
 <?
 require_once(dirname(__FILE__)."/classes_for_addons/SMagXXXdevice_class1.php");
-$adress = $this->getProperty("CONTROLADDRESS");
-$password = $this->getProperty("PASSWORD");
+$adress = $this->getProperty("ipaddres");
+$password = $this->getProperty("password");
 
 // Create Chromecast object and give IP and Port
 $mag = new MAG250();
